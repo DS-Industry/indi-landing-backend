5a727ed811194f344195adf2966a6fd9905cba16
bychenko-dmitriy <bychenko.dmitriy@vrn.moy-ka.ru>
2024-01-17 09:53:58 +0300

    add try/catch

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from "@nestjs/common";
 import { ConfigService } from '@nestjs/config';
 import { AxiosResponse } from 'axios';
 import {map, Observable, Subscription} from "rxjs";
+import crypto from "crypto";
 
 @Injectable()
 export class OrderService{
@@ -34,38 +35,69 @@ export class OrderService{
     }
 
     async check(data: any){
-        const body = data.orderId + '|' + data.response.razorpay_payment_id;
+        try {
+            const body = data.orderId + '|' + data.response.razorpay_payment_id;
 
-        const crypto = require('crypto');
-        const expectedSignature = crypto.createHmac('sha256', this.configService.get<string>('rp.key_secret'))
-            .update(body.toString())
-            .digest('hex');
-        let response = {'signatureIsValid':'false'}
-        if(expectedSignature === data.response.razorpay_signature) {
-            response = {'signatureIsValid': 'true'}
-            const options = this.setHeaders();
-            const requestObservable: Observable<any> = this.httpService.post(
-                `${this.configService.get<string>('dsCloudUrl')}/external/mobile/write/${data.deviceId}`,
-                {
-                    GVLCardNum: 0,
-                    GVLCardSum: data.amount,
-                    GVLSource: this.configService.get<string>('gvlSource'),
-                },
-                options
-            );
-            const subscription: Subscription = requestObservable.subscribe({
-                next: (response: AxiosResponse) => {
-                    // Обработка успешного ответа здесь
-                    console.log(response.data);
-                },
-                error: (error) => {
-                    // Обработка ошибки здесь
-                    console.error('Ошибка при выполнении http-запроса', error);
-                }
-            });
-        }else
-            response= {'signatureIsValid':'false'}
-        return response
+            const crypto = require('crypto');
+            const expectedSignature = crypto.createHmac('sha256', this.configService.get<string>('rp.key_secret'))
+                .update(body.toString())
+                .digest('hex');
+            let response = {'signatureIsValid': 'false'}
+            if (expectedSignature === data.response.razorpay_signature) {
+                response = {'signatureIsValid': 'true'}
+                const options = this.setHeaders();
+                const requestObservable: Observable<any> = this.httpService.post(
+                    `${this.configService.get<string>('dsCloudUrl')}/external/mobile/write/${data.deviceId}`,
+                    {
+                        GVLCardNum: 0,
+                        GVLCardSum: data.amount,
+                        GVLSource: this.configService.get<string>('gvlSource'),
+                    },
+                    options
+                );
+                const subscription: Subscription = requestObservable.subscribe({
+                    next: (response: AxiosResponse) => {
+                        console.log(response.data);
+                    },
+                    error: (error) => {
+                        console.error('Ошибка при выполнении http-запроса', error);
+                    }
+                });
+            } else
+                response = {'signatureIsValid': 'false'}
+            return response
+        }catch (err){
+            const body = data.orderId + '|' + data.response.razorpay_payment_id;
+
+            const crypto = require('crypto');
+            const expectedSignature = crypto.createHmac('sha256', this.configService.get<string>('rp.key_secret'))
+                .update(body.toString())
+                .digest('hex');
+            let response = {'signatureIsValid': 'false'}
+            if (expectedSignature === data.response.razorpay_signature) {
+                response = {'signatureIsValid': 'true'}
+                const options = this.setHeaders();
+                const requestObservable: Observable<any> = this.httpService.post(
+                    `${this.configService.get<string>('dsCloudUrl')}/external/mobile/write/${data.deviceId}`,
+                    {
+                        GVLCardNum: 0,
+                        GVLCardSum: data.amount,
+                        GVLSource: this.configService.get<string>('gvlSource'),
+                    },
+                    options
+                );
+                const subscription: Subscription = requestObservable.subscribe({
+                    next: (response: AxiosResponse) => {
+                        console.log(response.data);
+                    },
+                    error: (error) => {
+                        console.error('Ошибка при выполнении http-запроса', error);
+                    }
+                });
+            } else
+                response = {'signatureIsValid': 'false'}
+            return response
+        }
     }
 
     private setHeaders(): { headers: { akey: string } } {
