25d7725994cb35a084fe2f7e15c94c866757d913
bychenko-dmitriy <bychenko.dmitriy@vrn.moy-ka.ru>
2024-09-24 13:27:38 +0300

    add console log in check order

--- src/api/order/order.controller.ts
+++ src/api/order/order.controller.ts
@@ -45,6 +45,7 @@ export class OrderController {
   @HttpCode(201)
   async check(@Body() data: CheckOrderDto) {
     try {
+      console.log('check Order:' + data)
       return await this.orderUsecase.checkOrd(data);
     } catch (e) {
       throw new CustomHttpException({
